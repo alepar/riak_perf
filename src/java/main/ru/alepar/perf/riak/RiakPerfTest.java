cd98f9ac6ea43d1ddb3b4bb1f566c7f63e984f0f
Alexey Parfenov <alepar@gmail.com>
2015-05-05 22:04:44 -0700

    querying with random key from set

--- src/java/main/ru/alepar/perf/riak/RiakPerfTest.java
+++ src/java/main/ru/alepar/perf/riak/RiakPerfTest.java
@@ -2,11 +2,15 @@ package ru.alepar.perf.riak;
 
 import com.basho.riak.client.api.RiakClient;
 import com.basho.riak.client.api.commands.kv.FetchValue;
+import com.basho.riak.client.api.commands.kv.StoreValue;
 import com.basho.riak.client.core.query.Location;
 import com.basho.riak.client.core.query.Namespace;
 import com.basho.riak.client.core.query.RiakObject;
+import com.basho.riak.client.core.util.BinaryValue;
 import org.HdrHistogram.AtomicHistogram;
 import org.HdrHistogram.Histogram;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.alepar.perf.generator.JobGenerator;
 import ru.alepar.perf.generator.TpsGenerator;
 import ru.alepar.perf.generator.TpsGeneratorStatPrinter;
@@ -14,19 +18,55 @@ import ru.alepar.perf.generator.UnthrottledTpsGenerator;
 
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RiakPerfTest {
 
+    private static final Logger log = LoggerFactory.getLogger(RiakPerfTest.class);
+
     public static void main(String[] args) throws Exception {
-        final RiakClient client = RiakClient.newClient("127.0.0.1");
+        final String hostname = args[0];
+        final long nKeys = Long.valueOf(args[1]);
+        final int nThreads = Integer.valueOf(args[2]);
+
+        log.info("hostname {}; nKeys {}; nThreads {}", hostname, nKeys, nThreads);
+        final RiakClient client = RiakClient.newClient(hostname);
         final Namespace bucket = new Namespace("default", "my_bucket");
-        final Location key = new Location(bucket, "my_key");
+
+        final long warmupStart = System.nanoTime();
+        final long interval = (nKeys / nThreads) + 1;
+        final BinaryValue value = BinaryValue.create("my_value");
+        final Thread[] warmupThreads = new Thread[nThreads];
+        for(int i=0; i<nThreads; i++) {
+            final int ii = i;
+            warmupThreads[i] = new Thread(() -> {
+                for(long k=ii*interval; k<ii*interval+interval; k++) {
+                    final RiakObject storeObj = new RiakObject();
+
+                    storeObj.setValue(value);
+                    final Location key = new Location(bucket, "my_key"+k);
+                    final StoreValue store = new StoreValue.Builder(storeObj)
+                            .withLocation(key)
+                            .build();
+                    try {
+                        client.execute(store);
+                    } catch (Exception e) {
+                        log.warn("failed to warmup-write key", e);
+                    }
+                }
+            });
+            warmupThreads[i].start();
+        }
+        for(int i=0; i<nThreads; i++) {
+            warmupThreads[i].join();
+        }
+        log.info("warmup finished in {} seconds", (System.nanoTime()-warmupStart)/100_000_000/10.0);
 
         final Histogram histogram = new AtomicHistogram(1_000L, 1_000_000_000L, 4);
-        final JobGenerator readJobGenerator = new RiakReadJobGenerator(client, key, histogram);
+        final JobGenerator readJobGenerator = new RiakReadJobGenerator(client, bucket, nKeys, histogram);
 
         final TpsGenerator.Listener statPrinter = new TpsGeneratorStatPrinter(System.out);
-        final TpsGenerator tpsGenerator = new UnthrottledTpsGenerator(readJobGenerator, 8, statPrinter);
+        final TpsGenerator tpsGenerator = new UnthrottledTpsGenerator(readJobGenerator, nThreads, statPrinter);
         tpsGenerator.start();
 
         while(true) {
@@ -41,13 +81,15 @@ public class RiakPerfTest {
 
         private final Runnable runnable;
 
-        public RiakReadJobGenerator(RiakClient client, Location key, Histogram histogram) {
+        public RiakReadJobGenerator(RiakClient client, Namespace bucket, long nKeys, Histogram histogram) {
             runnable = () -> {
                 try {
+                    final Location key = new Location(bucket, "my_key"+ThreadLocalRandom.current().nextLong(nKeys));
                     final FetchValue fetch = new FetchValue.Builder(key).build();
                     final long start = System.nanoTime();
                     final FetchValue.Response response = client.execute(fetch);
-                    histogram.recordValue(System.nanoTime() - start);
+                    final long latency = System.nanoTime() - start;
+                    histogram.recordValue(latency);
                     final RiakObject fetchObj = response.getValue(RiakObject.class);
                     if (!"my_value".equals(fetchObj.getValue().toString())) {
                         throw new RuntimeException("assert failed; wrong value read: " + fetchObj.getValue().toString());
